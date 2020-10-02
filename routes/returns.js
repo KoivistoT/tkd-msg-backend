065f4b05ccf32e5c1fac20417041ad4336b5635f
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2020-10-02 11:20:40 +0300

    02102020 1120

--- routes/returns.js
+++ routes/returns.js
@@ -8,24 +8,18 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
   if (!rental) return res.status(404).send("Rental not found.");
 
   if (rental.dateReturned)
     return res.status(400).send("Return already processed");
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
-
-  await Movie.update({ _id: rental.movie._id }, { $inc: { numberInStock: 1 } });
-
+  rental.return();
   await rental.save();
 
-  return res.status(200).send(rental);
+  await Movie.update({ _id: rental.movie._id }, { $inc: { numberInStock: 1 } });
+  return res.send(rental);
 });
 
 function validateReturn(req) {
