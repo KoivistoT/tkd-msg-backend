99e0f873c48a0248183025b4e02a202c2d8b18fa
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-03-02 16:18:41 +0200

    removed unused functions

--- routes/rooms.js
+++ routes/rooms.js
@@ -131,52 +131,6 @@ router.post("/create_channel", auth, async (req, res) => {
   res.status(200).send(room);
 });
 
-router.post("/change_membership", auth, async (req, res) => {
-  // tähän validointi
-
-  let roomCheck = await Room.findOne({ _id: req.body.roomId });
-  if (!roomCheck) return res.status(400).send("Can't find room"); // tämä ei ehkä tarpeen, jos alussa validointi. toki aina parempi mitä enemmän varmuutta
-
-  const dbAction = req.body.membership ? "$addToSet" : "$pull";
-
-  try {
-    const updatedRoomData = await Room.findByIdAndUpdate(
-      { _id: req.body.roomId },
-      { [dbAction]: { members: req.body.userId } },
-      { new: true }
-    );
-
-    //tämä voisi olla jossain muualla functioissa., kuten muutkin, jottaon puhtaat nämä jutut täällä
-    User.updateOne(
-      { _id: req.body.userId },
-      { [dbAction]: { userRooms: req.body.roomId } },
-      async function (err, result) {
-        if (err) console.log(err);
-      }
-    );
-    const targetUsers = [req.body.userId];
-    const action = req.body.membership ? "roomAdded" : "roomRemoved";
-
-    //muutaa kyseisen käyttäjän huoneet, jotka näkyy
-    ioUpdateById(targetUsers, action, updatedRoomData);
-    //muuttaa kaikkien pääkäyttäjien controllin
-    ioUpdateToAllActiveUsers("controlMembersChanged", updatedRoomData, true);
-    //muuttaa kaikkien membersit, joilla on ks huone, paitsi target userin, joka on tuossa ekana
-    ioUpdateToAllActiveUsers(
-      "membersChanged",
-      updatedRoomData,
-      false,
-      req.body.userId
-    );
-    // ioUpdateToByRoomId([req.body.roomId], "membersChanged", updatedRoomData);
-
-    res.status(200).send(updatedRoomData);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send("something faild");
-  }
-});
-
 router.post("/change_members", auth, async (req, res) => {
   // tähän validointi
   const roomId = req.body.roomId;
@@ -283,15 +237,6 @@ router.post("/leave_room", auth, async (req, res) => {
   }
 });
 
-router.get("/all", auth, async (req, res) => {
-  const room = await Room.find({});
-  if (!room) return res.status(404).send("Rooms not found");
-
-  const roomsArray = addObjectIds(room);
-
-  res.status(200).send(roomsArray);
-});
-
 router.get("/delete_room/:id", async (req, res) => {
   const roomId = req.params.id;
 
@@ -360,14 +305,6 @@ router.post("/activate_room", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const result = await Room.find({ _id: req.params.id }); //.select("-messages");
   if (!result) return res.status(404).send("Room not found");
-
-  // res.send(_.pick(result[0], ["_id"]));
-  res.status(200).send(result[0]);
-});
-
-router.get("/members/:id", async (req, res) => {
-  const result = await Room.find({ _id: req.params.id }).select("members");
-  if (!result) return res.status(404).send("Members not found");
 
   // res.send(_.pick(result[0], ["_id"]));
   res.status(200).send(result[0]);
