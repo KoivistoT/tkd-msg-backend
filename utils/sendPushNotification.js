d8b042dee8a058dc151a8c4260bafbf2ae7a38f9
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-04-20 10:03:29 +0300

    Update sendPushNotification.js

--- utils/sendPushNotification.js
+++ utils/sendPushNotification.js
@@ -58,14 +58,9 @@ const sendPushMessages = async (allPushTokensData) => {
   //   ]);
 
   const sendChunks = async () => {
-    // This code runs synchronously. We're waiting for each chunk to be send.
-    // A better approach is to use Promise.all() and send multiple chunks in parallel.
-    // console.log("Sending Chunk", chunks);
     chunks.forEach(async (chunk) => {
-      // return;
       try {
         const tickets = await expo.sendPushNotificationsAsync(chunk);
-        // console.log("Tickets", tickets);
       } catch (error) {
         console.log("Error sending chunk", error);
       }
